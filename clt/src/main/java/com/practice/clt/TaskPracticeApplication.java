4047db577929a03a6f20a06d25c5b556eb20226d
Adam Ledesma <aledesma@clt.com.py>
2022-12-02 15:25:56 -0300

    clt api

---
+++ clt/src/main/java/com/practice/clt/TaskPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.clt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskPracticeApplication.class, args);
+	}
+
+}
